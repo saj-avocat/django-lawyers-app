74660776c1a4d091fecb3cbfb8583bb6f70e21cd
saj-avocat <saj-avocat>
2021-11-06 09:45:57 +0100

    add 8 apps in projects

---
+++ chikaya/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ChikayaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'chikaya'
---
+++ jonhi/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class JonhiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'jonhi'
